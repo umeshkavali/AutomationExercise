34943c0753af87bb6e562e580589548ae1a3d154
umeshkavali <umeshk@outlook.in>
2024-09-18 23:09:46 +0530

    Made Changes

--- src/test/java/pageObjects/CreateAUserPage.java
+++ src/test/java/pageObjects/CreateAUserPage.java
@@ -49,8 +49,11 @@ public class CreateAUserPage extends BasePage
 	
 	public void enterName(String nme)
 	{
-		txtName.clear();
-		txtName.sendKeys(nme);
+//		txtName.clear();
+//		txtName.sendKeys(nme);
+		
+		
+		
 	}
 	
 	
